2602e9be45f974473f1458a9fe521eb8470a83a9
willyfontenelle <willyfontenelle@gmail.com>
2023-06-14 15:54:05 -0300

    deleted readme.md

---
+++ .next/static/ldiXXe-X7LavqUS_wmtmm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0c428ae2-1df9a9182fa625b8.js","static/chunks/pages/index-729c20aacddc24f5.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
