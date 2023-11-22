069a218fb413113a6ad899073fa55650d3f4cf57
Willy Fontenelle <willyfontenelle@gmail.com>
2023-11-21 22:48:32 -0300

    using-image-component

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,12 @@ import {
 } from "react-icons/bs";
 import { FiFigma } from "react-icons/fi";
 import { useState } from "react";
-import avatar from "../public/avatar.jpg";
 import Image from "next/image";
 import Link from "next/link";
+import avatar from "../public/avatar.jpg";
+import suitcase from "../public/suitcase.png";
+import wine from "../public/wine.jpg";
+import shopfood from "../public/shopfood.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -38,11 +41,10 @@ export default function Home() {
               <ul className="flex items-center">
                 <li>
                   <div
-                    className={`cursor-pointer text-2xl ${
-                      darkMode
+                    className={`cursor-pointer text-2xl ${darkMode
                         ? "text-white hover:text-gray-200 active:text-gray-400"
                         : "text-gray-500 hover:text-gray-700 active:text-black"
-                    } transition-color duration-700 `}
+                      } transition-color duration-700 `}
                     onClick={() => setDarkMode(!darkMode)}
                     title="Switch mode"
                   >
@@ -51,11 +53,10 @@ export default function Home() {
                 </li>
                 <li>
                   <a
-                    className={`bg-blue-600 text-white py-3 px-4 border-none rounded-md ml-8 ${
-                      darkMode
+                    className={`bg-blue-600 text-white py-3 px-4 border-none rounded-md ml-8 ${darkMode
                         ? "hover:bg-blue-700 active:bg-blue-800"
                         : "hover:bg-blue-700 active:bg-blue-800"
-                    } focus:outline-none`}
+                      } focus:outline-none`}
                     href="/resumeWilly-en_compressed.pdf"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -86,11 +87,10 @@ export default function Home() {
                   rel="noopener noreferrer"
                 >
                   <span
-                    className={`cursor-pointer ${
-                      darkMode
+                    className={`cursor-pointer ${darkMode
                         ? "text-white hover:text-gray-200 active:text-gray-400"
                         : "text-gray-500 hover:text-gray-700 active:text-black"
-                    } transition-color duration-700`}
+                      } transition-color duration-700`}
                   >
                     <BsLinkedin />
                   </span>
@@ -101,11 +101,10 @@ export default function Home() {
                   rel="noopener noreferrer"
                 >
                   <span
-                    className={`cursor-pointer ${
-                      darkMode
+                    className={`cursor-pointer ${darkMode
                         ? "text-white hover:text-gray-200 active:text-gray-400"
                         : "text-gray-500 hover:text-gray-700 active:text-black"
-                    } transition-color duration-700`}
+                      } transition-color duration-700`}
                   >
                     <BsGithub />
                   </span>
@@ -116,11 +115,10 @@ export default function Home() {
                   rel="noopener noreferrer"
                 >
                   <span
-                    className={`cursor-pointer ${
-                      darkMode
+                    className={`cursor-pointer ${darkMode
                         ? "text-white hover:text-gray-200 active:text-gray-400"
                         : "text-gray-500 hover:text-gray-700 active:text-black"
-                    } transition-color duration-700`}
+                      } transition-color duration-700`}
                   >
                     <BsBehance />
                   </span>
@@ -131,11 +129,10 @@ export default function Home() {
                   rel="noopener noreferrer"
                 >
                   <span
-                    className={`cursor-pointer ${
-                      darkMode
+                    className={`cursor-pointer ${darkMode
                         ? "text-white hover:text-gray-200 active:text-gray-400"
                         : "text-gray-500 hover:text-gray-700 active:text-black"
-                    } transition-color duration-700`}
+                      } transition-color duration-700`}
                   >
                     <BsMedium />
                   </span>
@@ -146,11 +143,10 @@ export default function Home() {
                   rel="noopener noreferrer"
                 >
                   <span
-                    className={`cursor-pointer ${
-                      darkMode
+                    className={`cursor-pointer ${darkMode
                         ? "text-white hover:text-gray-200 active:text-gray-400"
                         : "text-gray-500 hover:text-gray-700 active:text-black"
-                    } transition-color duration-700`}
+                      } transition-color duration-700`}
                   >
                     <FiFigma />
                   </span>
@@ -161,7 +157,12 @@ export default function Home() {
             <div>
               <ul className="flex justify-center">
                 <li className="rounded-full w-56 h-56 relative overflow-hidden mb-16 md:h-80 md:w-80">
-                  <Image loader={imageLoader} src={avatar} alt="avatar" fill sizes="100vw" />
+                  <Image
+                    src={avatar}
+                  alt="avatar"
+                  fill
+                  sizes="100vw"
+                />
                 </li>
               </ul>
             </div>
@@ -180,8 +181,7 @@ export default function Home() {
                 <div className="md:flex md:flex-1 md:gap">
                   <div className="rounded-md overflow-hidden relative h-48 w-full object-cover md:w-1/2 md:h-auto">
                     <Image
-                      loader={imageLoader}
-                      src="/suitcase.png"
+                      src={suitcase}
                       alt="Suitcase Design System"
                       fill
                       sizes="100vw"
@@ -191,26 +191,26 @@ export default function Home() {
                       }} />
                   </div>
                   <div className="pt-4 md:pt-0 md:w-1/2 md:pl-4">
-                  <h3 className="text-lg font-medium pb-1 text-gray-800 dark:text-white">
-                    Suitcase Design System
-                  </h3>
-                  <p className="text-md text-gray-600 dark:text-gray-200">
-                    Creating the first version of a design system from theory to practice
-                  </p>
-                  <p className="text-gray-400 py-1 text-sm dark:text-gray-400">
-                    #designsystem #productmanagement #userinterface
-                    #userexperience #figma #designtokens #frontend-development
-                    #react
-                  </p>
-                  <a
-                    href="https://willyfontenelle.medium.com/suitcase-design-system-from-theory-to-practice-ba41c56c87ba"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 text-sm hover:underline"
-                  >
-                    Access project on Medium (English)
-                  </a>
-                </div>
+                    <h3 className="text-lg font-medium pb-1 text-gray-800 dark:text-white">
+                      Suitcase Design System
+                    </h3>
+                    <p className="text-md text-gray-600 dark:text-gray-200">
+                      Creating the first version of a design system from theory to practice
+                    </p>
+                    <p className="text-gray-400 py-1 text-sm dark:text-gray-400">
+                      #designsystem #productmanagement #userinterface
+                      #userexperience #figma #designtokens #frontend-development
+                      #react
+                    </p>
+                    <a
+                      href="https://willyfontenelle.medium.com/suitcase-design-system-from-theory-to-practice-ba41c56c87ba"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-600 text-sm hover:underline"
+                    >
+                      Access project on Medium (English)
+                    </a>
+                  </div>
                 </div>
               </div>
 
@@ -218,8 +218,7 @@ export default function Home() {
                 <div className="md:flex md:flex-1 md:gap">
                   <div className="rounded-md overflow-hidden relative h-48 w-full object-cover md:w-1/2 md:h-auto">
                     <Image
-                      loader={imageLoader}
-                      src="/wine.jpg"
+                      src={wine}
                       alt="Wine Eventos (We Wine)"
                       fill
                       sizes="100vw"
@@ -229,25 +228,25 @@ export default function Home() {
                       }} />
                   </div>
                   <div className="pt-4 md:pt-0 md:w-1/2 md:pl-4">
-                  <h3 className="text-lg font-medium pb-1 text-gray-800 dark:text-white">
-                  Wine Eventos (We Wine)
-                  </h3>
-                  <p className="text-md text-gray-600 dark:text-gray-200">
-                  Redesigning the experience of recommending drinks for events and monitoring its transition and results
-                  </p>
-                  <p className="text-gray-400 py-1 text-sm dark:text-gray-400">
-                    #userinterface #userexperience #designthinking #research #designsystem #figma
-                    #react
-                  </p>
-                  <a
-                    href="https://willyfontenelle.medium.com/wine-eventos-ux-case-bd47eea18fec"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 text-sm hover:underline"
-                  >
-                    Access project on Medium (Portuguese)
-                  </a>
-                </div>
+                    <h3 className="text-lg font-medium pb-1 text-gray-800 dark:text-white">
+                      Wine Eventos (We Wine)
+                    </h3>
+                    <p className="text-md text-gray-600 dark:text-gray-200">
+                      Redesigning the experience of recommending drinks for events and monitoring its transition and results
+                    </p>
+                    <p className="text-gray-400 py-1 text-sm dark:text-gray-400">
+                      #userinterface #userexperience #designthinking #research #designsystem #figma
+                      #react
+                    </p>
+                    <a
+                      href="https://willyfontenelle.medium.com/wine-eventos-ux-case-bd47eea18fec"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-600 text-sm hover:underline"
+                    >
+                      Access project on Medium (Portuguese)
+                    </a>
+                  </div>
                 </div>
               </div>
 
@@ -255,8 +254,7 @@ export default function Home() {
                 <div className="md:flex md:flex-1 md:gap">
                   <div className="rounded-md overflow-hidden relative h-48 w-full object-cover md:w-1/2 md:h-auto">
                     <Image
-                      loader={imageLoader}
-                      src="/shopfood.png"
+                      src={shopfood}
                       alt="Parmê & Shopfood"
                       fill
                       sizes="100vw"
@@ -266,24 +264,24 @@ export default function Home() {
                       }} />
                   </div>
                   <div className="pt-4 md:pt-0 md:w-1/2 md:pl-4">
-                  <h3 className="text-lg font-medium pb-1 text-gray-800 dark:text-white">
-                  Parmê & Shopfood
-                  </h3>
-                  <p className="text-md text-gray-600 dark:text-gray-200">
-                    Redesigning white-label e-commerce platform for restaurants and food deliveries
-                  </p>
-                  <p className="text-gray-400 py-1 text-sm dark:text-gray-400">
-                    #userinterface #userexperience #designsystem #figma
-                  </p>
-                  <a
-                    href="https://www.behance.net/gallery/98124739/Shopfood"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 text-sm hover:underline"
-                  >
-                    Access project on Behance
-                  </a>
-                </div>
+                    <h3 className="text-lg font-medium pb-1 text-gray-800 dark:text-white">
+                      Parmê & Shopfood
+                    </h3>
+                    <p className="text-md text-gray-600 dark:text-gray-200">
+                      Redesigning white-label e-commerce platform for restaurants and food deliveries
+                    </p>
+                    <p className="text-gray-400 py-1 text-sm dark:text-gray-400">
+                      #userinterface #userexperience #designsystem #figma
+                    </p>
+                    <a
+                      href="https://www.behance.net/gallery/98124739/Shopfood"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-600 text-sm hover:underline"
+                    >
+                      Access project on Behance
+                    </a>
+                  </div>
                 </div>
               </div>
 
